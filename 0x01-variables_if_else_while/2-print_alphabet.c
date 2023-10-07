ed9aacaeb58b20094fc864a8884928f1b88773af
lawalalx <lawal.alx@gmail.com>
2023-10-07 06:07:20 -0700

    test

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+
+putchar('\n');
+
+return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++) 
+{
+putchar(n + '0');
+if (n < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+
+putchar('\n');
+
+return (0);
+}
