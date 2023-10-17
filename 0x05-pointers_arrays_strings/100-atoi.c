274ad70d6f41a918e56b23c7832850e33dc34f34
lawalalx <lawal.alx@gmail.com>
2023-10-16 21:03:47 -0700

    test

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,19 +5,18 @@
  * @s: The input string.
  * Return: The converted integer.
  */
+
 int _atoi(char *s)
 {
 	int sign = 1;
 	int result = 0;
 	int i = 0;
 
-	// Handle leading spaces
 	while (s[i] == ' ')
 	{
 		i++;
 	}
 
-	// Handle signs
 	while (s[i] == '-' || s[i] == '+')
 	{
 		if (s[i] == '-')
@@ -27,7 +26,6 @@ int _atoi(char *s)
 		i++;
 	}
 
-	// Process digits
 	while (s[i] >= '0' && s[i] <= '9')
 	{
 		result = result * 10 + (s[i] - '0');
