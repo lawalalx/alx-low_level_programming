f237417cf98026ba65a4bca7e66e50b6e54d28dd
lawalalx <lawal.alx@gmail.com>
2023-10-19 01:09:45 -0700

    test

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+/**
+ * reverse_array - the function
+ * @a: arg
+ * @n: arg
+ * Return: void
+ */
+
+
+
+
+
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
+}
+
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+
+
+/**
+ * string_toupper - Converts all lowercase letters of a string to uppercase.
+ * @str: The string to convert.
+ *
+ * Return: A pointer to the resulting string.
+ */
+
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+
+	return (str);
+}
+
